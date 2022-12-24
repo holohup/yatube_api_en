d37e9d49466a7056d4c84cf3ca1b1b3729191797
Alexey Shapiro <academ@gmail.com>
2022-12-24 23:13:05 +0200

    added fixtures

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -7,10 +7,14 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'
+    empty_value_display = '-empty-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text')
 
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Follow)
--- yatube_api_install.sh
+++ yatube_api_install.sh
@@ -1,7 +1,8 @@
 python3 -m venv venv
 source venv/bin/activate
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 cd yatube_api
-python3 manage.py migrate
-python3 manage.py runserver
+python manage.py migrate
+python manage.py loaddata fixtures.json
+python manage.py runserver
